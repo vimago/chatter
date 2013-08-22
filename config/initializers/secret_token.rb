b64f698cebc83c7bcfcbac4b5fad00a6013f78db
Greg Molnar <molnargerg@gmail.com>
2013-08-22 07:21:53 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chatter::Application.config.secret_key_base = 'c343dcf3e68c6e9b231fbe8ab94144c87bc636cae98e62177c9e05e71573c88de5f6d591dd0779194924524b192212c8786dc19b10b67830521926e2574d5972'
