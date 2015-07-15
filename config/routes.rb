80b7e632eeae7982d004cabeb1fb4bf1788ce6db
Victor Gomez <victor@bluerocket.us>
2015-07-15 08:41:08 -0600

    Updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Chatter::Application.routes.draw do
   resources :messages
+
+  root 'messages#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -40,7 +43,7 @@ Chatter::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
